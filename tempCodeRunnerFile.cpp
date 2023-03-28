6c2efe1d6c4457ca768805a89f9fd4c8cf57ff91
canhcutbietbay <47494521+canhcutbietbay@users.noreply.github.com>
2023-03-29 01:22:03 +0700

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-    DauSach (char *isbn, char* ten, char *tg, char* tl, int st, int xb){
-		strcpy(ISBN, isbn);
-		strcpy(TenSach, ten);
-		SoTrang = st;
-		strcpy(TacGia, tg);
-		NXB = xb;
-		strcpy(TheLoai, tl);
-	}
