c1a81cf2dccbeec867e895206b90c08597efda63
SGTSuPo <sgtsupo@gmail.com>
2023-03-30 00:44:13 +0700

    Update StructDauSach.h

--- StructDauSach.h
+++ StructDauSach.h
@@ -4,11 +4,12 @@ struct DauSach
 {
     char ISBN[15], TenSach[25], TacGia[25], TheLoai[25];
     int SoTrang, NXB;
-	NodeSach *DS_Sach;
+    NodeSach *DS_Sach;
 
-	//constructor
-	DauSach() {}
-    DauSach (char isbn[], char tensach[], char tacgia[], char theloai[], int sotrang, int nxb ) {
+    // constructor
+    DauSach() {}
+    DauSach(char isbn[], char tensach[], char tacgia[], char theloai[], int sotrang, int nxb)
+    {
         strcpy(ISBN, isbn);
         strcpy(TenSach, tensach);
         strcpy(TacGia, tacgia);
@@ -17,14 +18,16 @@ struct DauSach
         NXB = nxb;
     }
 
-	//destructor
+    // destructor
     ~DauSach(){};
 };
-struct DS_DauSach{
-	int n;
-	DauSach *nodes[MAX_SIZE_LIST_DAU_SACH];
-	DS_DauSach(){
-		n = 0;
-	}
-	~DS_DauSach(){}
+struct DS_DauSach
+{
+    int n;
+    DauSach *nodes[MAX_SIZE_LIST_DAU_SACH];
+    DS_DauSach()
+    {
+        n = 0;
+    }
+    ~DS_DauSach() {}
 };
