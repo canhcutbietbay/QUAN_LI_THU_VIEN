6516e1a2f825300cad42318df2618cc3c881949f
SGTSuPo <sgtsupo@gmail.com>
2023-05-05 00:48:54 +0700

    Update StructTheDocGia.h

--- StructTheDocGia.h
+++ StructTheDocGia.h
@@ -1,4 +1,7 @@
-#include<math.h>
+#define MAX_SIZE_LIST_DOC_GIA 90000
+/*
+
+*/
 struct ArrRanMT
 {
 	bool used[90000];
@@ -69,10 +72,10 @@ struct TheDocGia
 		strcpy(Ten, ten);
 		Phai = phai;
 		TrangThai = trangthai;
+		DS_MT = NULL;
 	}
 };
-#define MAX_SIZE_LIST_DOC_GIA 90000
-
+//
 struct DSDocGia
 {
 	int n = 0;
@@ -88,23 +91,29 @@ struct DSDocGia
 			DSDG.n++;
 		}
 	}
-	int CompareDG(TheDocGia *a, TheDocGia *b){
-			if(strcmp(a->Ten, b->Ten) == 0){
-				return strcmp(a->Ho, b->Ho);
-			}else{
-				return strcmp(a->Ten, b->Ten);
+	int CompareDG(TheDocGia *a, TheDocGia *b)
+	{
+		if (strcmp(a->Ten, b->Ten) == 0)
+		{
+			return strcmp(a->Ho, b->Ho);
+		}
+		else
+		{
+			return strcmp(a->Ten, b->Ten);
 		}
 		return 0;
 	}
-	void SortDocGiaTheoTen(int l, int r)// Quick Sort
-	{ 
+	void SortDocGiaTheoTen(int l, int r) // Quick Sort
+	{
 		int i = l, j = r;
 		TheDocGia *pivot = nodes[(l + r) / 2];
 		TheDocGia *temp;
 		do
 		{
-			while (CompareDG(nodes[i], pivot) < 0) i++;
-			while (CompareDG(nodes[j], pivot) > 0) j--;
+			while (CompareDG(nodes[i], pivot) < 0)
+				i++;
+			while (CompareDG(nodes[j], pivot) > 0)
+				j--;
 			if (i <= j)
 			{
 				temp = nodes[i];
@@ -115,216 +124,234 @@ struct DSDocGia
 			}
 		} while (i <= j);
 
-		if (l < j) SortDocGiaTheoTen(l, j);
-		if (i < r) SortDocGiaTheoTen(i, r);
+		if (l < j)
+			SortDocGiaTheoTen(l, j);
+		if (i < r)
+			SortDocGiaTheoTen(i, r);
 	}
 };
+/*
 
-// Cay AVL
+*/
 struct NodeTheDocGia
 {
-	// key = DocGia.MaThe
 	TheDocGia DocGia;
-	int bf = 0;
-	NodeTheDocGia *left = nullptr;
-	NodeTheDocGia *right = nullptr;
+	NodeTheDocGia *left = NULL;
+	NodeTheDocGia *right = NULL;
+	int height = 0;
 };
-//
-// ham cua Cay AVL
-NodeTheDocGia *Rotate_Left(NodeTheDocGia *root)
-{
-	NodeTheDocGia *p;
-	if (root != nullptr)
-		if (root->right != nullptr)
-		{
-			p = root->right;
-			root->right = p->left;
-			p->left = root;
-		}
-	return p;
-}
-NodeTheDocGia *Rotate_Right(NodeTheDocGia *root)
-{
-	NodeTheDocGia *p;
-	if (root != nullptr)
-		if (root->left != nullptr)
-		{
-			p = root->left;
-			root->left = p->right;
-			p->right = root;
-		}
-	return p;
-}
-/*
-Them vao cay
-*/
-void InsertDocGia(NodeTheDocGia *&TreeAVLDocGia, TheDocGia *docgia)
-{
-	NodeTheDocGia *fp, *p, *q, *fya, *ya, *s;
-	int imbal;
-	fp = nullptr;
-	p = TreeAVLDocGia;
-	fya = nullptr;
-	ya = p;
 
-	while (p != nullptr)
+int max(int a, int b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
+int getHeight(NodeTheDocGia *root)
+{
+	if (root == NULL)
+		return 0;
+	// return 1 + max(getHeight(root->left), getHeight(root->right));
+	return root->height;
+}
+
+NodeTheDocGia *rightRotate(NodeTheDocGia *root)
+{
+	NodeTheDocGia *x = root->left;
+
+	// Bắt đầu quay phải
+	root->left = x->right;
+	x->right = root;
+
+	// Thiết lập chiều cao
+	root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+	x->height = 1 + max(getHeight(x->left), getHeight(x->right));
+
+	// Return x - trả về root mới
+	return x;
+}
+
+NodeTheDocGia *leftRotate(NodeTheDocGia *root)
+{
+	NodeTheDocGia *y = root->right;
+
+	// Bắt đầu quay trái
+	root->right = y->left;
+	y->left = root;
+
+	// Thiết lập chiều cao
+	root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+	y->height = 1 + max(getHeight(y->left), getHeight(y->right));
+
+	// Return y - trả về root mới
+	return y;
+}
+// Insertion - AVL Tree
+NodeTheDocGia *InsertDocGia(NodeTheDocGia *root, TheDocGia DocGia)
+{
+	// 1. Insert
+	if (root == NULL)
+		return new NodeTheDocGia{DocGia, NULL, NULL, 1}; // Trả về NodeTheDocGia có height = 1
+	if (DocGia.MaThe > root->DocGia.MaThe)
+		root->right = InsertDocGia(root->right, DocGia);
+	else if (DocGia.MaThe < root->DocGia.MaThe)
+		root->left = InsertDocGia(root->left, DocGia);
+	else
+		return root; // Neu bang thi khong them
+
+	// 2. Set height
+	root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+
+	// 3. Rotate
+	int valBalance = getHeight(root->left) - getHeight(root->right);
+
+	// Kiểm tra 4 TH xảy ra:
+	// 4.1. Left left
+	if (valBalance > 1 && DocGia.MaThe < root->left->DocGia.MaThe)
+		return rightRotate(root);
+
+	// 4.2. Right Right
+	if (valBalance < -1 && DocGia.MaThe > root->right->DocGia.MaThe)
+		return leftRotate(root);
+
+	// 4.3. Left Right
+	if (valBalance > 1 && DocGia.MaThe > root->left->DocGia.MaThe)
 	{
-		if (docgia->MaThe == p->DocGia.MaThe) // bi trung khoa
-			return;
-		if (docgia->MaThe < p->DocGia.MaThe)
-			q = p->left;
-		else
-			q = p->right;
-		if (q != nullptr)
-			if (q->bf != 0) // truong hop chi so can bang cua q la 1 hay -1
+		root->left = leftRotate(root->left);
+		return rightRotate(root);
+	}
+
+	// 4.4. Right Left
+	if (valBalance < -1 && DocGia.MaThe < root->right->DocGia.MaThe)
+	{
+		root->right = rightRotate(root->right);
+		return leftRotate(root);
+	}
+
+	return root;
+}
+
+// Giá trị cân bằng - Balance
+int Balance(NodeTheDocGia *root)
+{
+	if (root == NULL)
+		return 0;
+	return getHeight(root->left) - getHeight(root->right);
+}
+
+NodeTheDocGia *maxMaThe(NodeTheDocGia *root)
+{
+	NodeTheDocGia *current = root;
+
+	// Tìm kiếm NodeTheDocGia có mã thẻ lớn nhất
+	while (current->right != NULL)
+		current = current->right;
+
+	return current;
+}
+
+// Deletion - AVL Tree
+NodeTheDocGia *deleteNode(NodeTheDocGia *root, TheDocGia DocGia)
+{
+	// 1. XÓA NODE - DELETE
+	if (root == NULL)
+		return root;
+
+	if (DocGia.MaThe > root->DocGia.MaThe)
+		root->right = deleteNode(root->right, DocGia);
+	else if (DocGia.MaThe < root->DocGia.MaThe)
+		root->left = deleteNode(root->left, DocGia);
+
+	// Nếu MaThe có giá trị bằng với root->DocGia.MaThe
+	// Thì đây chính là NodeTheDocGia cần xóa
+	else
+	{
+		// Trường hợp 0 con hoặc 1 con
+		if (root->left == NULL || root->right == NULL)
+		{
+			// Sử dụng NodeTheDocGia temp để kiểm tra
+			NodeTheDocGia *temp = root->left;
+			if (root->right != NULL)
+				temp = root->right;
+
+			// TH: 0 con - No child case
+			if (temp == NULL)
 			{
-				fya = p;
-				ya = q;
+				temp = root;
+				root = NULL;
+				delete temp;
 			}
-		fp = p;
-		p = q;
-	}
-	// Thêm nút mới (nut la) la con cua nut fp
-	q = new NodeTheDocGia;
-	q->DocGia.MaThe = docgia->MaThe;
-	strcpy(q->DocGia.Ho, docgia->Ho);
-	strcpy(q->DocGia.Ten, docgia->Ten);
-	q->DocGia.Phai = docgia->Phai;
-	q->DocGia.TrangThai = docgia->TrangThai;
-	q->DocGia.DS_MT = nullptr;
-	q->bf = 0;
-	q->left = nullptr;
-	q->right = nullptr;
-	if (docgia->MaThe < fp->DocGia.MaThe)
-		fp->left = q;
-	else
-		fp->right = q;
+			// TH: 1 con - One child case
+			else
+			{
+				// Gán tất cả các giá trị (bao gồm left, right, height) của temp vào root
+				*root = *temp;
+				delete temp;
+			}
+		}
+		// Trường hợp 2 con - Two child case
+		else
+		{
+			// Tìm NodeTheDocGia lớn nhất bên trái (nhỏ nhất bên phải)
+			// successor - biggest in the left subtree
+			NodeTheDocGia *temp = maxMaThe(root->left);
 
-	/*Hieu chinh chi so can bang cua tat ca cac nut giua ya va q, neu bi lech
-		 ve phia trai thi chi so can bang cua tat ca cac nut giua ya va q deu la
-		 1, neu bi lech ve phia phai thi chi so can bang cua tat ca cac nut giua
-		 ya va q deu la -1 */
-	if (docgia->MaThe < ya->DocGia.MaThe)
-		p = ya->left;
-	else
-		p = ya->right;
-	s = p; // s là con của nút ya
-	while (p != q)
-	{
-		if (docgia->MaThe < p->DocGia.MaThe)
-		{
-			p->bf = 1;
-			p = p->left;
-		}
-		else
-		{
-			p->bf = -1;
-			p = p->right;
+			// Đưa DocGia của temp vào root
+			root->DocGia = temp->DocGia;
+
+			// Xóa temp như 2 TH trên - Delete the inorder successor
+			root->right = deleteNode(root->right, temp->DocGia);
 		}
 	}
-	// xac dinh huong lech
-	if (docgia->MaThe < ya->DocGia.MaThe)
-		imbal = 1;
-	else
-		imbal = -1;
-	// cay vua lech
-	if (ya->bf == 0)
+
+	// Nếu không còn gì thì trả về luôn !!
+	if (root == NULL)
+		return root;
+
+	// 2. CẬP NHẬT CHIỀU CAO - UPDATE HEIGHT OF THE CURRENT NODE
+	root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+
+	// 3. CÂN BẰNG CÂY - GET THE BALANCE FACTOR
+	int valBalance = Balance(root);
+
+	// Kiểm tra 4 TH xảy ra - There are 4 cases
+
+	// Case 1: Left left - Trái trái
+	if (valBalance > 1 && Balance(root->left) >= 0)
+		return rightRotate(root);
+
+	// Case 2: Right right - Phải phải
+	if (valBalance < -1 && Balance(root->right) <= 0)
+		return leftRotate(root);
+
+	// Case 3: Left right - Trái phải
+	if (valBalance > 1 && Balance(root->left) < 0)
 	{
-		ya->bf = imbal;
-		return;
+		root->left = leftRotate(root->left);
+		return rightRotate(root);
 	}
-	// cay het lech
-	if (ya->bf != imbal)
+
+	// Case 4: Right left - Phải trái
+	if (valBalance < -1 && Balance(root->right) > 0)
 	{
-		ya->bf = 0;
-		return;
+		root->right = rightRotate(root->right);
+		return leftRotate(root);
 	}
-	// cây mất cân bằng
-	if (s->bf == imbal) // Truong hop xoay don
-	{
-		if (imbal == 1) // xoay phai
-		{
-			p = Rotate_Right(ya);
-		}
-		else // xoay trai
-		{
-			p = Rotate_Left(ya);
-		}
-		ya->bf = 0;
-		s->bf = 0;
-	}
-	else // Truong hop xoay kep
-	{
-		if (imbal == 1) // xoay kep trai-phai
-		{
-			ya->left = Rotate_Left(s);
-			p = Rotate_Right(ya);
-		}
-		else // xoay kep phai-trai -
-		{
-			ya->right = Rotate_Right(s);
-			p = Rotate_Left(ya);
-		}
-		if (p->bf == 0) // truong hop p la nut moi them vao
-		{
-			ya->bf = 0;
-			s->bf = 0;
-		}
-		else if (p->bf == imbal)
-		{
-			ya->bf = -imbal;
-			s->bf = 0;
-		}
-		else
-		{
-			ya->bf = 0;
-			s->bf = imbal;
-		}
-		p->bf = 0;
-	}
-	if (fya == nullptr) TreeAVLDocGia = p;
-	else if (ya == fya->right) fya->right = p;
-	else fya->left = p;
-}
-/*
-TAO CAY TIM KIEM AVL
-*/
-NodeTheDocGia *Create_AVLTree(TheDocGia *root) // tao cay khi nhap data tu file
-{
-	NodeTheDocGia *fr = new NodeTheDocGia;
-	//...
-	if (root != nullptr)
-	{
-		fr->DocGia.MaThe = root->MaThe;
-		strcpy(fr->DocGia.Ho, root->Ho);
-		strcpy(fr->DocGia.Ten, root->Ten);
-		fr->DocGia.Phai = root->Phai;
-		fr->DocGia.TrangThai = root->TrangThai;
-		fr->DocGia.DS_MT = nullptr;
-		fr->bf = 0;
-		fr->left = nullptr;
-		fr->right = nullptr;
-		return fr;
-	}
-	//	else
-	//		return;
-}
-/*
-XOA NUT CAY AVL
-*/
-void RemoveDocGia(NodeTheDocGia *TreeAVLDocGia, int mathe)
-{
+
+	return root;
 }
 /*
 HIEU CHINH THONG TIN DOC GIA
 */
 
-int treeLevel(NodeTheDocGia *TreeAVLDocGia){
-	if (TreeAVLDocGia == NULL) return -1;
-	return 1 + std::max(treeLevel(TreeAVLDocGia->left), treeLevel(TreeAVLDocGia->right));
-}
-bool checkAvl(NodeTheDocGia *TreeAVLDocGia){
-	if (TreeAVLDocGia == NULL) 	return true;
-	if (std::abs(treeLevel(TreeAVLDocGia->left) - treeLevel(TreeAVLDocGia->right)) > 1) return false;
-	return checkAvl(TreeAVLDocGia->left) && checkAvl(TreeAVLDocGia->right);
+void InDocGiaTheoMaDocGia(NodeTheDocGia *&TreeAVLDocGia) // Inorder
+{
+	if (TreeAVLDocGia != nullptr)
+	{
+		InDocGiaTheoMaDocGia(TreeAVLDocGia->left);
+		cout << TreeAVLDocGia->DocGia.MaThe << "||" << TreeAVLDocGia->DocGia.Ho << "||" << TreeAVLDocGia->DocGia.Ten << "||" << TreeAVLDocGia->DocGia.Phai << "||" << TreeAVLDocGia->DocGia.TrangThai << endl;
+		InDocGiaTheoMaDocGia(TreeAVLDocGia->right);
+	}
 }
