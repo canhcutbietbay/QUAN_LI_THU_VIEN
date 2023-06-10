669b8fe9f6b66759f2dfb127fe0571b5d2949bb4
SGTSuPo <sgtsupo@gmail.com>
2023-06-11 00:06:20 +0700

    Update StructDanhMucSach.h

--- StructDanhMucSach.h
+++ StructDanhMucSach.h
@@ -6,11 +6,20 @@ struct Sach
   int TrangThai;
   char ViTri[15];
   // construction
+  Sach(){};
+  Sach(char ms[], int tt, char vt[])
+  {
+    strcpy(MaSach, ms);
+    TrangThai = tt;
+    strcpy(ViTri, vt);
+  }
+
+  ~Sach() {}
 };
 // lien ket don
 struct DM_Sach
 {
-  Sach sach;
+  Sach *sach;
   DM_Sach *next;
   ~DM_Sach() {}
 };
@@ -72,7 +81,7 @@ void InsertLast_DM_Sach(DM_Sach *First, Sach &sach)
 DM_Sach *GetDM_Sach(DM_Sach *First, int id)
 {
   DM_Sach *node = First;
-  while (node->sach.id < id) // ds tinh tu 1
+  while (node->sach->id < id) // ds tinh tu 1
     if (node->next == nullptr)
       return nullptr;
     else
@@ -81,22 +90,22 @@ DM_Sach *GetDM_Sach(DM_Sach *First, int id)
 }
 
 // lay node sach theo ma sach
-// DM_Sach *GetDM_Sach(DM_Sach *First, char *masach)
-// {
-//   for (DM_Sach *node = First; node != nullptr; node = node->next)
-//   {
-//     if (strcmp(node->sach.MaSach, masach) == 0)
-//       return node;
-//   }
-//   return nullptr;
-// }
+DM_Sach *GetDM_Sach(DM_Sach *First, char *masach)
+{
+  for (DM_Sach *node = First; node != nullptr; node = node->next)
+  {
+    if (strcmp(node->sach.MaSach, masach) == 0)
+      return node;
+  }
+  return nullptr;
+}
 
 // lay sach theo ma sach
 Sach *GetSach(DM_Sach *First, char *masach)
 {
   DM_Sach *node = GetDM_Sach(First, masach);
   if (node)
-    return &node->sach;
+    return node->sach;
   else
     return nullptr;
 }
@@ -105,91 +114,95 @@ Sach *GetSach(DM_Sach *First, char *masach)
 void DeleteDM_Sach(DM_Sach *First, char *masach)
 {
 }
-
+void DeleteDM_Sach(DM_Sach *&node)
+{
+  delete node;
+}
 // tao MaSach tu dong
-DM_Sach *CreateMaSach(DauSach &ds, int &addval)
-{
-  DauSach *node = ds;
-  // TH dausach chua co sach
-  if (node->soluong == 0)
-  {
-    node->DS_Sach = CreateDM_Sach();
-    node->DS_Sach->sach.id = 1;
-    // them 0 khi id be hon 10
-    if (node->DS_Sach->sach.id < 10)
-    {
-      char *s;
-      itoa(node->DS_Sach->sach.id, s);
-      s = "0" + s;
-      node->DS_Sach->sach.MaSach = node->ISBN + s;
-    }
-    else
-    {
-      char *s;
-      itoa(node->DS_Sach->sach.id, s);
-      node->DS_Sach->sach.MaSach = node->ISBN + s;
-    }
-    addval--;
-    return node->DS_Sach;
-  }
-  // TH them o cuoi
-  else if (node->DS_Sach->next == nullptr)
-  {
-    node->DS_Sach->next = CreateDM_Sach();
-    node->DS_Sach->next->sach.id = node->DS_Sach->sach.id + 1;
-    //
-    if (node->DS_Sach->sach.id < 9)
-    {
-      char *s;
-      itoa(node->DS_Sach->next->sach.id, s);
-      s = "0" + s;
-      node->DS_Sach->next->sach.MaSach = node->ISBN + s;
-    }
-    else
-    {
-      char *s;
-      itoa(node->DS_Sach->next->sach.id, s);
-      node->DS_Sach->next->sach.MaSach = node->ISBN + s;
-    }
-    addval--;
-    return node->DS_Sach->next;
-  }
-  // TH khuyet id vi bi xoa tu truoc
-  else if (node->DS_Sach->next->sach.id - node->DS_Sach->sach.id != 1)
-  {
-    DM_Sach *newnode = CreateDM_Sach();
-    newnode->next = node->DS_Sach->next;
-    node->DS_Sach->next = newnode;
-    newnode->sach.id = node->DS_Sach->sach.id + 1;
-    //
-    if (newnode->sach.id < 9)
-    {
-      char *s;
-      itoa(node->DS_Sach->next->sach.id, s);
-      s = "0" + s;
-      node->DS_Sach->next->sach.MaSach = node->ISBN + s;
-    }
-    else
-    {
-      char *s;
-      itoa(node->DS_Sach->next->sach.id, s);
-      node->DS_Sach->next->sach.MaSach = node->ISBN + s;
-    }
-    addval--;
-    return node->DS_Sach->next;
-  }
-}
+// DM_Sach *CreateMaSach(DauSach &ds, int &addval)
+// {
+//   DauSach *node = ds;
+//   // TH dausach chua co sach
+//   if (node->soluong == 0)
+//   {
+//     node->DS_Sach = CreateDM_Sach();
+//     node->DS_Sach->sach.id = 1;
+//     // them 0 khi id be hon 10
+//     if (node->DS_Sach->sach.id < 10)
+//     {
+//       char *s;
+//       itoa(node->DS_Sach->sach.id, s);
+//       s = "0" + s;
+//       node->DS_Sach->sach.MaSach = node->ISBN + s;
+//     }
+//     else
+//     {
+//       char *s;
+//       itoa(node->DS_Sach->sach.id, s);
+//       node->DS_Sach->sach.MaSach = node->ISBN + s;
+//     }
+//     addval--;
+//     node->soluong = 1;
+//     return node->DS_Sach;
+//   }
+//   // TH them o cuoi
+//   else if (node->DS_Sach->next == nullptr)
+//   {
+//     node->DS_Sach->next = CreateDM_Sach();
+//     node->DS_Sach->next->sach.id = node->DS_Sach->sach.id + 1;
+//     //
+//     if (node->DS_Sach->sach.id < 9)
+//     {
+//       char *s;
+//       itoa(node->DS_Sach->next->sach.id, s);
+//       s = "0" + s;
+//       node->DS_Sach->next->sach.MaSach = node->ISBN + s;
+//     }
+//     else
+//     {
+//       char *s;
+//       itoa(node->DS_Sach->next->sach.id, s);
+//       node->DS_Sach->next->sach.MaSach = node->ISBN + s;
+//     }
+//     addval--;
+//     return node->DS_Sach->next;
+//   }
+//   // TH khuyet id vi bi xoa tu truoc
+//   else if (node->DS_Sach->next->sach.id - node->DS_Sach->sach.id != 1)
+//   {
+//     DM_Sach *newnode = CreateDM_Sach();
+//     newnode->next = node->DS_Sach->next;
+//     node->DS_Sach->next = newnode;
+//     newnode->sach.id = node->DS_Sach->sach.id + 1;
+//     //
+//     if (newnode->sach.id < 9)
+//     {
+//       char *s;
+//       itoa(node->DS_Sach->next->sach.id, s);
+//       s = "0" + s;
+//       node->DS_Sach->next->sach.MaSach = node->ISBN + s;
+//     }
+//     else
+//     {
+//       char *s;
+//       itoa(node->DS_Sach->next->sach.id, s);
+//       node->DS_Sach->next->sach.MaSach = node->ISBN + s;
+//     }
+//     addval--;
+//     return node->DS_Sach->next;
+//   }
+// }
 
-// Nhap thong tin sach theo so luong sach nhap vao dau sach
-void NhapSach(DauSach &ds, int &addval)
-{
-  DM_Sach *node = ds.DM_Sach;
-  while (addval > 0)
-  {
-    node = CreateMaSach(ds, addval);
-    /*
-    Them vao trang thai va vi tri
-    TH nguoi dung nhan HUY thi dung lai
-    */
-  }
-}
+// // Nhap thong tin sach theo so luong sach nhap vao dau sach
+// void NhapSach(DauSach &ds, int &addval)
+// {
+//   DM_Sach *node = ds.DM_Sach;
+//   while (addval > 0)
+//   {
+//     node = CreateMaSach(ds, addval);
+//     /*
+//     Them vao trang thai va vi tri
+//     TH nguoi dung nhan HUY o MaSach nao thi xoa MS do roi dung lai
+//     */
+//   }
+// }
