417fc382e7da5624c77d3b639c6018b54d1ecce1
SGTSuPo <sgtsupo@gmail.com>
2023-06-12 15:05:45 +0700

    Update StructDanhMucSach.h
    Hàm xóa DM_Sach (sử dụng khi hủy quá trình nhập tt sách khi thêm sách vào đầu sách

--- StructDanhMucSach.h
+++ StructDanhMucSach.h
@@ -1,5 +1,5 @@
-// #ifndef _STRUCTDANHMUCSACH_H // Nếu chưa định nghĩa _STRUCTDANHMUCSACH_H 
-// #define _STRUCTDANHMUCSACH_H // thì định nghĩa _STRUCTDANHMUCSACH_H 
+// #ifndef _STRUCTDANHMUCSACH_H // Nếu chưa định nghĩa _STRUCTDANHMUCSACH_H
+// #define _STRUCTDANHMUCSACH_H // thì định nghĩa _STRUCTDANHMUCSACH_H
 // #include "StructDauSach.h"
 #define MAX_SIZE_LIST_DMS 100
 struct Sach
@@ -105,96 +105,24 @@ DM_Sach *GetDM_Sach(DM_Sach *First, int id)
 //}
 
 // xoa sach theo ma sach
-void DeleteDM_Sach(DM_Sach *First, char *masach)
+void DeleteDM_Sach(DM_Sach *&First, char *masach)
 {
+  DM_Sach *node = First;
+  if (strcmp(First->sach->id, masach) == 0)
+  {
+    First = nullptr;
+    delete node;
+  }
+  else
+    for (node->next != nullptr, node = node->next)
+    {
+      if (strcmp(node->next->sach->id, masach) == 0)
+      {
+        DM_Sach *nodedel = node->next;
+        node->next = node->next->next;
+        delete nodedel;
+      }
+    }
 }
-
-// // tao MaSach tu dong
-// DM_Sach *CreateMaSach(DauSach &ds, int &addval)
-// {
-//   DauSach *node = ds;
-//   // TH dausach chua co sach
-//   if (node->soluong == 0)
-//   {
-//     node->DS_Sach = CreateDM_Sach();
-//     node->DS_Sach->sach.id = 1;
-//     // them 0 khi id be hon 10
-//     if (node->DS_Sach->sach.id < 10)
-//     {
-//       char *s;
-//       itoa(node->DS_Sach->sach.id, s);
-//       s = "0" + s;
-//       node->DS_Sach->sach.MaSach = node->ISBN + s;
-//     }
-//     else
-//     {
-//       char *s;
-//       itoa(node->DS_Sach->sach.id, s);
-//       node->DS_Sach->sach.MaSach = node->ISBN + s;
-//     }
-//     addval--;
-//     return node->DS_Sach;
-//   }
-//   // TH them o cuoi
-//   else if (node->DS_Sach->next == nullptr)
-//   {
-//     node->DS_Sach->next = CreateDM_Sach();
-//     node->DS_Sach->next->sach.id = node->DS_Sach->sach.id + 1;
-//     //
-//     if (node->DS_Sach->sach.id < 9)
-//     {
-//       char *s;
-//       itoa(node->DS_Sach->next->sach.id, s);
-//       s = "0" + s;
-//       node->DS_Sach->next->sach.MaSach = node->ISBN + s;
-//     }
-//     else
-//     {
-//       char *s;
-//       itoa(node->DS_Sach->next->sach.id, s);
-//       node->DS_Sach->next->sach.MaSach = node->ISBN + s;
-//     }
-//     addval--;
-//     return node->DS_Sach->next;
-//   }
-//   // TH khuyet id vi bi xoa tu truoc
-//   else if (node->DS_Sach->next->sach.id - node->DS_Sach->sach.id != 1)
-//   {
-//     DM_Sach *newnode = CreateDM_Sach();
-//     newnode->next = node->DS_Sach->next;
-//     node->DS_Sach->next = newnode;
-//     newnode->sach.id = node->DS_Sach->sach.id + 1;
-//     //
-//     if (newnode->sach.id < 9)
-//     {
-//       char *s;
-//       itoa(node->DS_Sach->next->sach.id, s);
-//       s = "0" + s;
-//       node->DS_Sach->next->sach.MaSach = node->ISBN + s;
-//     }
-//     else
-//     {
-//       char *s;
-//       itoa(node->DS_Sach->next->sach.id, s);
-//       node->DS_Sach->next->sach.MaSach = node->ISBN + s;
-//     }
-//     addval--;
-//     return node->DS_Sach->next;
-//   }
-// }
-
-// // Nhap thong tin sach theo so luong sach nhap vao dau sach
-// void NhapSach(DauSach &ds, int &addval)
-// {
-//   DM_Sach *node = ds.DM_Sach;
-//   while (addval > 0)
-//   {
-//     node = CreateMaSach(ds, addval);
-//     /*
-//     Them vao trang thai va vi tri
-//     TH nguoi dung nhan HUY thi dung lai
-//     */
-//   }
-// }
 
 // #endif
