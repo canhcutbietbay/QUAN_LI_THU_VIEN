e21b0cdbc2c793226b2aa852db08431ec078360a
canhcutbietbay <47494521+canhcutbietbay@users.noreply.github.com>
2023-06-14 16:41:53 +0700

    .

--- test.cpp
+++ test.cpp
@@ -7,6 +7,7 @@ struct DateTime
 {
     // char Year[3], Month[3], Day[3];
     int Year, Month, Day;
+    char res[15];
     DateTime(){};
     DateTime(int year, int month, int day)
     {
@@ -20,7 +21,22 @@ struct DateTime
     //     strcpy(Month, month);
     //     strcpy(Day, day);
     // }
-
+    void GetRes()
+    {
+        char temp[10];
+        itoa(Day, temp, 10);
+        if (Day < 10)
+            strcat(this->res, "0");
+        strcat(this->res, temp);
+        strcat(this->res, "/");
+        if (Month < 10)
+            strcat(this->res, "0");
+        itoa(Month, temp, 10);
+        strcat(this->res, temp);
+        strcat(this->res, "/");
+        itoa(Year, temp, 10);
+        strcat(this->res, temp);
+    }
     void GetTimeNow()
     {
         time_t CurrentTime = time(0);
@@ -28,6 +44,7 @@ struct DateTime
         Year = Time->tm_year + 1900;
         Month = Time->tm_mon + 1;
         Day = Time->tm_mday;
+        GetRes();
     }
 
     void GetSoNgayMuon()
@@ -40,7 +57,7 @@ struct DateTime
         Start->tm_year = Year - 1900;
         Start->tm_mon = Month - 1;
         Start->tm_mday = Day;
-        unsigned long long Seconds = difftime(CurrentTime,mktime(Start));
+        unsigned long long Seconds = difftime(CurrentTime, mktime(Start));
         int SoNgayMuon = Seconds / 86400;
         cout << SoNgayMuon << endl;
     }
@@ -50,5 +67,8 @@ int main()
 {
     DateTime Time(2021, 5, 23);
     Time.GetSoNgayMuon();
+    DateTime Now;
+    Now.GetTimeNow();
+    cout << Now.res << endl;
     return 0;
 }
